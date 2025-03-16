a57713a3e35dfbac39f5aae401362347f7f90929
Kalyaan-vs <subash.kalyaan@gmail.com>
2025-03-16 14:14:20 +0530

    initial commit

--- open_url.py
+++ open_url.py
@@ -4,11 +4,11 @@
 # chrome = webbrowser.get(chrome_path)
 # chrome.open("https://learnprogramming.academy/courses")
 
+if __name__ == "__main__":
+    import webbrowser
 
-import webbrowser
+    # webbrowser.open("https://docs.python.org/3.10/library/webbrowser")
 
-# webbrowser.open("https://docs.python.org/3.10/library/webbrowser")
-
-# chrome = webbrowser.get(using="google-chrome")
-chrome = webbrowser.get(r"'C:\Program Files\Google\Chrome\Application\chrome.exe' %s")
-chrome.open("https://learnprogramming.academy/courses")
+    # chrome = webbrowser.get(using="google-chrome")
+    chrome = webbrowser.get(r"'C:\Program Files\Google\Chrome\Application\chrome.exe' %s")
+    chrome.open("https://learnprogramming.academy/courses")
